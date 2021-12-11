9e71e0966f2a92b6b920eb0bd1f169efbd28824b
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2021-12-11 20:01:24 +0600

    Adding digital fonts and updating animation
    Adding Orbitron font for watch face and heart rate monitor. Adding AM - PM below the time and BPM with the heart rate monitor. Heart rate updating from one to five seconds. Adding leading zero for one digit numbers ( 0 - 9).
    Heart animation updated to growing effect with formal opacity fading. Opacity updated from 0.5 to 0.75
    Adding more details commenting in JavaScript file.

--- Project-001-SmartWatch/main.js
+++ Project-001-SmartWatch/main.js
@@ -13,6 +13,7 @@ const showTime = document.querySelector(".show-time");
 const showHeart = document.querySelector(".show-heart");
 const heartImage = document.querySelector(".heart-image");
 
+//DISPLAYING DIFFERENT WATCH FACES ON COLOR BUTTON CLICK
 redButton.addEventListener("click", () => {
   watchImage.src = "./images/red.png";
 });
@@ -28,9 +29,25 @@ pinkButton.addEventListener("click", () => {
 purpleButton.addEventListener("click", () => {
   watchImage.src = "./images/purple.png";
 });
+
+//ADDING ZERO BRFORE ONE DIGIT NUMBERS (0-9)
+let addZero = function (number) {
+  if (number <= 9) {
+    return "0" + number;
+  }
+  return number;
+};
+
+//UPDATING THE TIME EVERY SECOND
 setInterval(() => {
   let date = new Date();
+  let amPm = "";
   let hour = date.getHours();
+  if (hour < 12) {
+    amPm = "AM";
+  } else {
+    amPm = "PM";
+  }
   if (hour === 0) {
     hour = 12;
   }
@@ -39,11 +56,23 @@ setInterval(() => {
   }
   let minute = date.getMinutes();
   let second = date.getSeconds();
-  showTime.innerHTML = hour + " : " + minute + " : " + second;
-
-  showHeart.innerHTML = Math.floor(Math.random() * (100 - 60 + 1)) + 60;
+  showTime.innerHTML =
+    addZero(hour) +
+    " : " +
+    addZero(minute) +
+    " : " +
+    addZero(second) +
+    "<br/>" +
+    amPm;
 }, 1000);
 
+//UPDATING THE HEART COUNT EVERY FIVE SECONDS
+setInterval(() => {
+  showHeart.innerHTML =
+    Math.floor(Math.random() * (100 - 60 + 1)) + 60 + " BPM";
+}, 5000);
+
+//ADDING ONCLICK FUNCTIONALITIES ON TIME AND HEART RATE BUTTON
 timeButton.addEventListener("click", () => {
   showTime.classList.remove("display-none");
   showHeart.classList.add("display-none");
